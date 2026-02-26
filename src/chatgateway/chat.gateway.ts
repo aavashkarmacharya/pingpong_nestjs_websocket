fa46513da1bb69046c65021c6b116e78260d4e40
aavash <karmacharya.aavash0@gmail.com>
2026-02-26 12:04:11 +0545

    added vertical movement to both paddles with nestjs websocket implementation for said function and basic ball events being worked on

--- src/chatgateway/chat.gateway.ts
+++ src/chatgateway/chat.gateway.ts
@@ -1,10 +1,13 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { SpannerDriver } from 'typeorm/driver/spanner/SpannerDriver.js';
+import { PlainObjectToNewEntityTransformer } from 'typeorm/query-builder/transformer/PlainObjectToNewEntityTransformer.js';
 
 @WebSocketGateway({
   cors: {
@@ -16,10 +19,65 @@ export class websocketgateway
 {
   @WebSocketServer()
   server: Server;
+  private gamestate = {
+    player1: {
+      x: 15,
+      y: 45,
+      width: 10,
+      height: 50,
+      speed: 0,
+    },
+    player2: {
+      x: 975,
+      y: 45,
+      width: 10,
+      height: 50,
+      speed: 0,
+    },
+    ball: {
+      x: 151,
+      y: 65,
+      radius: 7,
+      speedX: 5,
+      speedY: 8,
+    },
+  };
   handleConnection(client: Socket) {
     console.log(`Player has joined! ${client.id}`);
   }
   handleDisconnect(client: Socket) {
     console.log(`Player has disconnected! ${client.id}`);
+  }
+  @SubscribeMessage('paddleMove')
+  handlePaddle(client: Socket, data: { player: number; direction: string }) {
+    const player = data.player;
+    const direction = data.direction;
+    if (player === 1) {
+      if (direction === 'up') {
+        this.gamestate.player1.speed = -2;
+      }
+      if (direction === 'down') {
+        this.gamestate.player1.speed = 2;
+      }
+      if (direction === 'stop') {
+        this.gamestate.player1.speed = 0;
+      }
+      this.gamestate.player1.y += this.gamestate.player1.speed;
+    }
+    if (player === 2) {
+      if (direction === 'up') {
+        this.gamestate.player2.speed = -2;
+      }
+      if (direction === 'down') {
+        this.gamestate.player2.speed = 2;
+      }
+      if (direction === 'stop') {
+        this.gamestate.player2.speed = 0;
+      }
+
+      this.gamestate.player2.y += this.gamestate.player2.speed;
+    }
+    console.log(`Player ${player} has moved!`);
+    this.server.emit('gamestate', this.gamestate);
   }
 }
--- src/public/index.js
+++ src/public/index.js
@@ -1,67 +1,142 @@
-function ConnectTOServer() {
-  const socket = io('http://localhost:3000/');
+function ConnectToServer() {
+  const socket = io('http://localhost:3000');
   socket.on('connect', () => {
-    console.log('connected to server!');
+    console.log('Connected to server!', socket.id);
+    setupKeyEvents(socket);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from server');
   });
   return socket;
 }
-Socket = ConnectTOServer();
+const socket = ConnectToServer();
 
+socket.on('gamestate', (event) => {
+  console.log('movement detected!');
+  player1.y = event.player1.y;
+  player2.y = event.player2.y;
+  ball.x = event.ball.x;
+  ball.y = event.ball.y;
+});
+//drawing stuff
 const pingpong = document.getElementById('pingpong');
 const ctx = pingpong.getContext('2d');
-const score = document.getElementById('score');
-const start = document.getElementById('start-btn');
+
 const ball = {
-  x: pingpong.width / 2,
+  x: pingpong.width / 2 + 1,
   y: pingpong.height / 2,
-  radius: 5,
+  radius: 7,
   speedX: 5,
   speedY: 8,
   color: '#dde8e3',
 };
-const player1 = {
-  x: 10,
-  y: pingpong.height / 2,
-  width: 10,
-  height: 50,
-  color: '#e9e3e3c4',
-};
-const player2 = {
-  x: pingpong.width - 10,
-  y: pingpong.height / 2,
-  width: 10,
-  height: 50,
-  color: '#e9e3e3c4',
-};
-function scorechange() {}
-function makerect(x, y, width, height) {}
-ctx.fillrect(x, y, width, height);
-ctx.fillstyle = color;
 
-function makecircle(x, y, radius, color) {
-  ctx.beginpath();
-  ctx.fillstyle = color;
+const player1 = {
+  x: 5,
+  y: pingpong.height / 2 - 25,
+  width: 10,
+  height: 50,
+  color: '#e9e3e3c4',
+};
+
+const player2 = {
+  x: pingpong.width - 15,
+  y: pingpong.height / 2 - 25,
+  width: 10,
+  height: 50,
+  color: '#e9e3e3c4',
+};
+
+function makeRect(x, y, width, height, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+}
+function createLine() {
+  ((ctx.fillStyle = '#ede6e6c0'),
+    ctx.fillRect(pingpong.width / 2, 0, 2, pingpong.height));
+}
+function makeCircle(x, y, radius, color) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
   ctx.arc(x, y, radius, 0, Math.PI * 2);
   ctx.fill();
 }
 
-//key_Events
-function getkeyevents(Socket) {
+keys = {
+  player1up: false,
+  player1down: false,
+  player2up: false,
+  player2down: false,
+};
+//key events registration
+function setupKeyEvents(socket) {
+  const keys = {
+    player1up: false,
+    player1down: false,
+    player2up: false,
+    player2down: false,
+  };
   document.addEventListener('keydown', (event) => {
-    if (event.key === 'w' || event.key === 'ArrowUp') {
-      handlekeyupevents();
+    const key = event.key.toLowerCase();
+    if (key === 'w') {
+      keys.player1up = true;
+      console.log('Moving up player: 1');
+      socket.emit('paddleMove', { player: 1, direction: 'up' });
     }
-    if (event.key === 'S' || event.key === 'ArrowDown') {
-      handlekeydownevents();
+    if (key === 'arrowup') {
+      keys.player2up = true;
+      console.log('moving  up player 2');
+      socket.emit('paddleMove', { player: 2, direction: 'up' });
+    }
+
+    if (key === 's') {
+      keys.player1down = true;
+      console.log('Moving down player: 1');
+      socket.emit('paddleMove', { player: 1, direction: 'down' });
+    }
+    if (key === 'arrowdown') {
+      keys.player2down = true;
+      console.log('moving down player: 2');
+      socket.emit('paddleMove', { player: 2, direction: 'down' });
+    }
+    w;
+  });
+  document.addEventListener('keyup', (event) => {
+    const key = event.key.toLowerCase();
+    if (key === 'w') {
+      keys.player1up = false;
+      console.log('Stop moving player: 1');
+      socket.emit('paddleMove', { player: 1, direction: 'stop' });
+    }
+    if (key === 'arrowup') {
+      keys.player2up = false;
+      console.log('Stop moving player: 2');
+      socket.emit('paddleMove', { player: 1, direction: 'stop' });
+    }
+    if (key === 's') {
+      keys.player1down = false;
+      console.log('stop moving player: 1');
+      socket.emit('paddleMove', { player: 2, direction: 'stop' });
+    }
+    if (key === 'arrowdown') {
+      keys.player2down = false;
+      console.log('stop moving player: 2');
+      socket.emit('paddleMove', { player: 2, direction: stop });
     }
   });
 }
-
-function handlekeyupevents() {
-  console.log('key up !');
+function draw() {
+  ctx.fillStyle = '#4848cc';
+  ctx.fillRect(0, 0, pingpong.width, pingpong.height);
+  ctx.beginPath();
+  createLine();
+  makeRect(player1.x, player1.y, player1.width, player1.height, player1.color);
+  makeRect(player2.x, player2.y, player2.width, player2.height, player2.color);
+  makeCircle(ball.x, ball.y, ball.radius, ball.color);
 }
-function handlekeydownevents() {
-  console.log('key down!');
+function gameLoop() {
+  draw();
+  requestAnimationFrame(gameLoop);
 }
-
-getkeyevents();
+gameLoop();
